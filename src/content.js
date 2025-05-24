6bba4fb2d271de113f238bf846e439012e7200aa
Connor <connorlwhite@icloud.com>
2025-05-24 09:24:59 -0400

    Trying to block enter key press in serach bar

--- src/content.js
+++ src/content.js
@@ -5,3 +5,18 @@ document.body.style.backgroundColor = "pink";
 document.querySelectorAll("img").forEach(img => {
   img.src = "https://placekitten.com/300/300";
 });
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    const active = document.activeElement;
+    console.log("Blocking search query");
+    // Check if the focused element is the Google search input
+    if (active && active.name === "q") {
+      e.preventDefault();
+      console.log("Blocked Enter key for Google search bar");
+
+      const query = active.value;
+      chrome.runtime.sendMessage({ type: "query-blocked", query });
+    }
+  }
+});
